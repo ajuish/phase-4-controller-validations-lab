d6928d9ba68319326f38c086df73f1b1396f90d8
Alex Ju <aju217@gmail.com>
2022-11-24 20:08:47 -0500

    commit tree b2500eb8f34aef03d213a36d99c74b8b12972ba0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_response
+
+    private
+    def render_invalid_response(invalid)
+        render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    end
 end
